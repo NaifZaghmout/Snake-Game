171ad73ab6d28e6d138f78ddb6b2fe707b46052b
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2023-07-18 13:34:30 +0000

    add apple_cllision and place_apple

--- run.py
+++ run.py
@@ -8,13 +8,13 @@ def print_filed():
     output = ''
     for cell in cells:
         if cell in snake_body:
-            output += fore.GREEN + 'X'
+            output += fore.GREEN + 'x'
 
         elif cell == apple_Pos:
-            output += fore.RED + 'A'
+            output += fore.RED + '*'
 
         elif cell[1] in (0, FIELD_HEIGHT - 1) or cell[0] in (0, FIELD_WIDTH - 1):
-            output += fore.CYAN + '#'
+            output += fore.CYAN + '-'
 
         else:
             output += ''
@@ -36,6 +36,41 @@ def update_snake():
     if not has_eaten:
         snake_body.pop[-1]
     has_eaten = False
+
+
+
+
+
+def apple_cllision():
+    global apple_Pos , has_eaten
+
+    if snake_body[0] == apple_Pos:
+        apple_Pos = place_apple()
+        has_eaten = True
+
+
+
+
+
+
+def place_apple():
+    col = randint(1 , FIELD_WIDTH - 2)
+    row = randint(1 , FIELD_HEIGHT - 2)
+    while (col , row) in snake_body:
+        col= randint(1 , FIELD_WIDTH - 2)
+        row = randint(1 , FIELD_HEIGHT - 2)
+    return (col , row)
+
+
+
+
+init(autorest=True)
+
+
+FIELD_WIDTH = 25
+FIELD_HEIGHT = 15
+cells = [(col , row) for row in range (FIELD_HEIGHT) for col in range (FIELD_WIDTH)]
+
 
 
 
