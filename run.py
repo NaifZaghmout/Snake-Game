27755d5e2a19597166c443596ece6f7dc9ab6b1e
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2023-07-18 09:41:31 +0000

    add def update_snake

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from pytimedinput import timedInput
 from random import randint
 import os
-from colorama import fore , init
+from colorama import Fore, init
 
 
 def print_filed():
@@ -13,14 +13,29 @@ def print_filed():
         elif cell == apple_Pos:
             output += fore.RED + 'A'
 
-        elif cell[1] in (0 , FIELD_HEIGHT -1 )  or cell[0] in (0, FIELD_WIDTH -1):
+        elif cell[1] in (0, FIELD_HEIGHT - 1) or cell[0] in (0, FIELD_WIDTH - 1):
             output += fore.CYAN + '#'
 
         else:
-             output += ''
+            output += ''
 
-        if cell[0] == FIELD_WIDTH -1 :
-            output +- '/n'    
+        if cell[0] == FIELD_WIDTH - 1:
+            output + - '/n'
 
 
-    print(output)        
+
+    print(output)
+
+
+
+
+def update_snake():
+    global has_eaten
+    new_head = snake_body[0][0] + direction[0], snake_body[0][1] + direction[1]
+    snake_body.insert(0, new_head)
+    if not has_eaten:
+        snake_body.pop[-1]
+    has_eaten = False
+
+
+
