9eb40c81da90e989e90780c6fa8c806c7b54756f
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2023-07-18 08:52:58 +0000

    add import , from and first def print_filed

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from pytimedinput import timedInput
+from random import randint
+import os
+from colorama import fore , init
+
+
+def print_filed():
+    output = ''
+    for cell in cells:
+        if cell in snake_body:
+            output += fore.GREEN + 'X'
+
+        elif cell == apple_Pos:
+            output += fore.RED + 'A'
+
+        elif cell[1] in (0 , FIELD_HEIGHT -1 )  or cell[0] in (0, FIELD_WIDTH -1):
+            output += fore.CYAN + '#'
+
+        else:
+             output += ''
+
+        if cell[0] == FIELD_WIDTH -1 :
+            output +- '/n'    
+
+
+    print(output)        
